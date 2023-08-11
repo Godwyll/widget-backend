e6ebc356330bdce689a7182fb49b013a38a8d5f3
Godwyll <a.godwyll@gmail.com>
2023-08-11 07:51:48 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# WIDGET BACKEND
+
+POP-UP BEHAVIOUR
+- Randomize Popup visibility (Modal)
+- Shuffle content 
+- - Track User IP
+
+# MODELS
+- Content 
+- Question 
+- Option 
+- Answer 
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(UserSeeder::class);
+        // $this->call(ContentSeeder::class);
+        // $this->call(QuestionSeeder::class);
+        // $this->call(OptionSeeder::class);
+        // $this->call(AnswerSeeder::class);
+    }
+}
